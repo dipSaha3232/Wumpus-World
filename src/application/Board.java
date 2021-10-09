8563e4e54e456ae3dea602148a14bd45a590e832
Dip Saha <bsse1001@iit.du.ac.bd>
2021-10-09 12:10:16 +0600

    wumpus world

---
+++ src/application/Board.java
@@ -0,0 +1,13 @@
+package application;
+
+public class Board {
+	
+	boolean [][] pit = new boolean [10][10];
+	boolean [][] breeze = new boolean [10][10];
+	boolean [][] wumpus = new boolean [10][10];
+	boolean [][] stench = new boolean [10][10];
+	boolean [][] visited = new boolean [10][10];
+	boolean [][] glitter = new boolean [10][10];
+	boolean [][] ok = new boolean [10][10];
+
+}
